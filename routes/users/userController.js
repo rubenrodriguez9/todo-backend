413401f69e6b6453a4e478e43ac5e366aa2f0958
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-11-20 15:52:59 -0700

    installing jwt-decode

--- routes/users/userController.js
+++ routes/users/userController.js
@@ -42,23 +42,20 @@ module.exports = {
         try{
             console.log(req.body)
             let foundEmail = await User.findOne({email: req.body.email})
-
-        
+            let comparedPassword = await bcrypt.compare(req.body.password, foundEmail.password)
+        //checking to see if user exists
             if(!foundEmail){
-
-                throw {message: "user not found ", status: 409} 
-            } else {
-                let comparedPassword = await bcrypt.compare(req.body.password, foundEmail.password)
-
-                if(!comparedPassword){
+                throw {message: "user not found ", status: 404}
+        //checking to see if the password matches the found user
+            } else if (!comparedPassword){
                     throw {message: "incorrect password", status: 401}
-                    
-                }
-            }
-            
+               
+            } 
+            // here we are creating a token that expires in one hour
             const token =  jwt.sign({email: foundEmail.email, _id: foundEmail._id}, "hamsteroverlord007", {expiresIn: "1h"})
             console.log(token);
-            
+            // now we are sending back that token
+            res.json({jwtToken: token})
         }
         catch (e) {
             console.log(e);
